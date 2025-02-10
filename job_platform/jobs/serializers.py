d727cb52c6dd7a73858be925f739b3e79e5ddb49
Zhan1bek <zhanibekmyrzakhan@gmail.com>
2025-02-10 14:32:20 +0500

    add serializers, viewsets and API

---
+++ job_platform/jobs/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Vacancy, Resume, Request, Review
+
+class VacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
+
+class ResumeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resume
+        fields = '__all__'
+
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
--- job_platform/jobs/views.py
+++ job_platform/jobs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vacancy, Resume, Request, Review
+from .serializers import VacancySerializer, ResumeSerializer, RequestSerializer, ReviewSerializer
 
-# Create your views here.
+class VacancyViewSet(viewsets.ModelViewSet):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+class ResumeViewSet(viewsets.ModelViewSet):
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
+
+class RequestViewSet(viewsets.ModelViewSet):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
