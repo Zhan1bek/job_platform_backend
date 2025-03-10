225b57b78ec2052d23a6d927ca2aa8e0efc7c0b4
aslanmyn <mustafaev.aslan@55gmail.com>
2025-03-10 12:00:00 +0500

    swagger,redoc

--- job_platform/jobs/views.py
+++ job_platform/jobs/views.py
@@ -2,12 +2,29 @@ from rest_framework import viewsets
 from .models import Vacancy, Resume, Request, Review
 from .serializers import VacancySerializer, ResumeSerializer, RequestSerializer, ReviewSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets
+from .models import Vacancy
+from .serializers import VacancySerializer
 
 class VacancyViewSet(viewsets.ModelViewSet):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['company', 'work_type', 'region']
+
+    @swagger_auto_schema(
+        operation_description="Получить список вакансий",
+        responses={200: VacancySerializer(many=True)},
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Создать новую вакансию",
+        request_body=VacancySerializer,
+        responses={201: VacancySerializer},
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 class ResumeViewSet(viewsets.ModelViewSet):
     queryset = Resume.objects.all()
     serializer_class = ResumeSerializer
